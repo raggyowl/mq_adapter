d450b5eb5d4d3a0045c1360fb6cee75918501d73
exaveb <walkohm@gmail.com>
2018-11-09 23:06:09 +0300

    Add comment for Adapter inerface

--- adapter/mq.go
+++ adapter/mq.go
@@ -1,5 +1,8 @@
 package adapter
 
+
+//Adapter is an interface that describes behavior to an object that consumes
+ //and publishes messages from/to a queues and delivers them to recipients.
 type Adapter interface {
 	Fetch(routingKey, exchange, url string) error
 	Dispatch(routingKey, contentType, exchange string, data []byte) error
